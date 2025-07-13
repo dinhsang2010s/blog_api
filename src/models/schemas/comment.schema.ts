e77fe0d57e0c22ef12a0b9950ab0b43d9b2f271e
dinhsang2010s <nguyendinhsang.it98@gmail.com>
2025-07-14 01:51:27 +0700

    add-index-comment

--- src/models/schemas/comment.schema.ts
+++ src/models/schemas/comment.schema.ts
@@ -4,7 +4,7 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 export class Comment {
   @Prop({ required: true }) postId: string;
   @Prop({ required: true }) userId: string;
-  @Prop({ default: null }) parentCommentId?: string;
+  @Prop({ index: true, default: null }) parentCommentId?: string;
   @Prop({ required: true }) content: string;
   @Prop({ min: 1, max: 5 }) rating?: number;
   @Prop({ default: 0 }) likes: number;
